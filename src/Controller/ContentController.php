90bf9ee186b6c7265accab4636528620b4c56d14
Altigar <altigar01@gmail.com>
2021-06-19 19:08:35 +0700

    serializer fix

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -30,7 +30,7 @@ class ContentController extends AbstractController
     {
 	    $questions = $this->entityManager->getRepository(Question::class)->findBy(['survey' => $survey], ['ordering' => 'asc']);
 	    if ($request->isXmlHttpRequest()) {
-	    	return $this->json($questions);
+	    	return $this->json($questions, context: [AbstractNormalizer::IGNORED_ATTRIBUTES => ['answers']]);
 	    }
 	    return $this->render('content/index.html.twig', [
 		    'title' => 'Content',
