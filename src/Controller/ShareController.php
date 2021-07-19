6ecdbf608873b5d9d3d382389017f1e078b4044a
Altigar <altigar01@gmail.com>
2021-07-19 13:49:07 +0700

    Share page add

---
+++ src/Controller/ShareController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Survey;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShareController extends AbstractController
+{
+    #[Route('/share/{survey}', name: 'share', methods: ['GET'])]
+    public function index(Survey $survey): Response
+    {
+        return $this->render('share/index.html.twig', [
+        	'title' => 'Share',
+	        'survey' => $survey->getId()
+        ]);
+    }
+}
