9525ca7570ec48898a6a9ddbd80e8c00cd55044e
Altigar <altigar01@gmail.com>
2021-08-29 23:26:15 +0700

    refactor sql stats queries

--- src/Repository/AnswerRepository.php
+++ src/Repository/AnswerRepository.php
@@ -23,12 +23,11 @@ class AnswerRepository extends ServiceEntityRepository
 
 	public function findNoteStatsBySurvey(Survey $survey): array
 	{
-		$sql = <<<SQL
+		$sql = <<<'SQL'
 			SELECT question_id, answer.text
 			FROM answer
 			JOIN question ON question.id = answer.question_id
-			JOIN survey ON question.survey_id = survey.id
-			WHERE survey_id = :survey AND (question.type = :text OR question.type = :string)
+			WHERE question.survey_id = :survey AND (question.type = :text OR question.type = :string)
 			GROUP BY question_id, answer.text
 		SQL;
 		$rsm = (new ResultSetMapping())
@@ -44,12 +43,11 @@ class AnswerRepository extends ServiceEntityRepository
 
 	public function findChoiceStatsBySurvey(Survey $survey): array
 	{
-		$sql = <<<SQL
+		$sql = <<<'SQL'
 			SELECT question_id, option_id, count(*) AS amount, sum(count(*)) OVER(partition by question_id) as total
 			FROM answer
 			JOIN question ON question.id = answer.question_id
-			JOIN survey ON question.survey_id = survey.id
-			WHERE survey_id = :survey AND (question.type = :radio OR question.type = :checkbox)
+			WHERE question.survey_id = :survey AND (question.type = :radio OR question.type = :checkbox)
 			GROUP BY question_id, option_id
 		SQL;
 		$rsm = (new ResultSetMapping())
@@ -69,13 +67,13 @@ class AnswerRepository extends ServiceEntityRepository
 
 	public function findScaleStatsBySurvey(Survey $survey): array
 	{
-		$sql = '
+		$sql = <<<'SQL'
 			SELECT question_id, scale_value, count(*) AS amount, sum(count(*)) OVER(partition by question_id) as total
 			FROM answer
 			JOIN question ON question.id = answer.question_id
 			WHERE question.survey_id = :survey AND question.type = :scale
 			GROUP BY question_id, scale_value
-		';
+		SQL;
 		$rsm = (new ResultSetMapping())
 			->addScalarResult('id', 'id')
 			->addScalarResult('question_id', 'question_id')
